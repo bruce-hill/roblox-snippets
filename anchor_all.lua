139e33254bf5b5aa316ceca7949934943ee6d120
bruce-hill <bruce@bruce-hill.com>
2021-09-01 16:52:23 -0700

    Update anchor_all.lua

--- anchor_all.lua
+++ anchor_all.lua
@@ -1,7 +1,8 @@
 return "Anchor all", "Anchor all descendants of selection", function()
 	for _,desc in ipairs(game.Selection:Get()[1]:GetDescendants()) do
 		if desc:IsA("BasePart") and not desc.Anchored then
-			print("Unanchored:", desc:GetFullName())
+			print("Anchoring "..desc:GetFullName())
+			desc.Anchored = true
 		end
 	end
 end
