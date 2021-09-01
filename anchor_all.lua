eadcf9c82778788a59334c718689bd7d762fe114
bruce-hill <bruce@bruce-hill.com>
2021-09-01 16:22:07 -0700

    Create anchor_all.lua

---
+++ anchor_all.lua
@@ -0,0 +1,7 @@
+return "Anchor all descendants of selection", function()
+	for _,desc in ipairs(game.Selection:Get()[1]:GetDescendants()) do
+		if desc:IsA("BasePart") and not desc.Anchored then
+			print("Unanchored:", desc:GetFullName())
+		end
+	end
+end
